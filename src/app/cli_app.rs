25d4b9b06c061f9b07121fd12acc2b385b011d68
myFavShrimp <myFavShrimp@outlook.de>
2021-08-26 13:31:02 +0200

    Added password prompt to CliApp

--- src/app/cli_app.rs
+++ src/app/cli_app.rs
@@ -3,6 +3,8 @@ use std::path::PathBuf;
 use super::ClaveApp;
 use super::cryptor;
 
+use rpassword::read_password_from_tty;
+
 /// Clave console application struct.
 #[derive(Debug)]
 pub struct CliApp {
@@ -23,8 +25,15 @@ impl ClaveApp for CliApp {
         println!("These are the paths you have selected for processing:");
         &self.file_paths.iter().for_each(|item| println!("\"{}\"", item.display()));
 
-        let mut cipher = cryptor::create_cipher("some key".as_bytes());
+        let mut password = match CliApp::prompt_password() {
+            Some(password) => { password }
+            _ => {
+                println!("No input. Application is exited.");
+                std::process::exit(0);
+            }
+        };
 
+        let mut cipher = cryptor::create_cipher(password.as_bytes());
         let mut encryption_results = vec![];
 
         for path in &self.file_paths {
@@ -48,5 +57,45 @@ impl ClaveApp for CliApp {
                 }
             }
         }
+    }
+}
+
+const READ_PASSWORD_ERROR_MESSAGE: &'static str = "Could not read the password. Trying again ...";
+const PASSWORD_MATCH_ERROR_MESSAGE: &'static str = "The passwords do not match. Trying again ...";
+
+const CHOOSE_PASSWORD_MESSAGE: &'static str
+    = "Choose a password to use for processing (leave empty to exit): ";
+const CONFIRM_PASSWORD_MESSAGE: &'static str
+    = "Confirm your password: ";
+
+impl CliApp {
+    /// Prompts the user to choose a password and reads the users response.
+    fn prompt_password() -> Option<String> {
+        let mut password: Option<String> = None;
+
+        while password.is_none() {
+            match read_password_from_tty(Some(CHOOSE_PASSWORD_MESSAGE)) {
+                Ok(input) => {
+                    if !&input.is_empty() {
+                        match read_password_from_tty(Some(CONFIRM_PASSWORD_MESSAGE)) {
+                            Ok(input_confirm) => {
+                                if input == input_confirm {
+                                    password = Some(input);
+                                }
+                                else {
+                                    println!("{}", PASSWORD_MATCH_ERROR_MESSAGE);
+                                }
+                            }
+                            _ => { println!("{}", READ_PASSWORD_ERROR_MESSAGE); }
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+                _ => { println!("{}", READ_PASSWORD_ERROR_MESSAGE); }
+            }
+        }
+        password
     }
 }
