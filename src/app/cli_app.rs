42728f0633d8185c16a250954370a6046278edd8
myFavShrimp <myFavShrimp@outlook.de>
2021-08-26 12:57:50 +0200

    Improved CliApp output

--- src/app/cli_app.rs
+++ src/app/cli_app.rs
@@ -17,16 +17,36 @@ impl ClaveApp for CliApp {
     }
 
     fn run(&mut self) {
-        println!("Running!");
-        println!("{:#?}", self.file_paths);
-
         self.file_paths.sort();
         self.file_paths.dedup();
 
+        println!("These are the paths you have selected for processing:");
+        &self.file_paths.iter().for_each(|item| println!("\"{}\"", item.display()));
+
         let mut cipher = cryptor::create_cipher("some key".as_bytes());
 
+        let mut encryption_results = vec![];
+
         for path in &self.file_paths {
-            println!("{:?}", cryptor::encrypt_path(&mut cipher, path));
+            encryption_results.extend(cryptor::encrypt_path(&mut cipher, path));
+        }
+
+        println!("Finished!");
+        if encryption_results.iter().any(|item| item.is_ok()) {
+            println!("The following files were processed successfully:");
+            for item in &encryption_results {
+                if let Ok(file_path) = item {
+                    println!("  {}", file_path.display());
+                }
+            }
+        }
+        if encryption_results.iter().any(|item| item.is_ok()) {
+            println!("Errors occurred during the processing of the following files:");
+            for item in &encryption_results {
+                if let Err((file_path, error_message)) = item {
+                    println!("  {} : {}", error_message, file_path.display());
+                }
+            }
         }
     }
 }
