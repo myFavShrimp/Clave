0e237cc001f6f20135e81b486c083660b897fb8a
myFavShrimp <myFavShrimp@outlook.de>
2021-08-26 10:20:46 +0200

    Adjusted clap app

--- src/app/args.rs
+++ src/app/args.rs
@@ -3,11 +3,10 @@ use clap::{App, AppSettings, Arg, crate_name, crate_version, crate_authors};
 /// Returns the clap app of the application.
 pub fn get_clap_app() -> App<'static, 'static> {
     App::new(crate_name!())
-        .about("Encrypt your files to share them securely.")
+        .about("Encrypts your files in place to share them securely.")
         .version(crate_version!())
         .author(crate_authors!())
         .setting(AppSettings::ArgRequiredElseHelp)
-        .after_help("Tip: you can use `e` or `d` instead of `encrypt` and `decrypt`")
         .arg(
             Arg::with_name("files").value_name("files")
                 .help("Files to process").required(true).multiple(true))
