262feab9fa0d578b87ca59e210cdd1456f3cbb5a
myFavShrimp <42263661+myFavShrimp@users.noreply.github.com>
2023-06-02 09:02:41 +0000

    Created library method entrypoint and new module for password prompting

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,24 @@ use cli_app::PasswordPromptError;
 pub mod args;
 pub mod cli_app;
 mod cryptor;
+mod password;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("error setting up logging output")]
     LoggerError(#[from] log::SetLoggerError),
     #[error("error reading password: {0}")]
-    PasswordPromptError(#[from] PasswordPromptError),
+    PasswordPromptError(#[from] password::PasswordPromptError),
     #[error("error collecting arguments: {0}")]
     ClapError(#[from] clap::Error),
+}
+
+pub fn process(paths: Vec<std::path::PathBuf>) -> Result<(), Error> {
+    let password = password::prompt_password()?;
+
+    let mut cipher = cryptor::create_cipher(password.as_bytes());
+
+    
+    
+    Ok(())
 }
---
+++ src/password.rs
@@ -0,0 +1,21 @@
+static CHOOSE_PASSWORD_MESSAGE: &str = "Choose a password to use for processing: ";
+static CONFIRM_PASSWORD_MESSAGE: &str = "Confirm your password: ";
+
+#[derive(Debug, thiserror::Error)]
+pub enum PasswordPromptError {
+    #[error("Error reading password")]
+    IoError(#[from] std::io::Error),
+    #[error("the passwords do not match")]
+    MatchError,
+}
+
+pub fn prompt_password() -> Result<String, PasswordPromptError> {
+    let password = rpassword::prompt_password(CHOOSE_PASSWORD_MESSAGE)?;
+    let password_confirm = rpassword::prompt_password(CONFIRM_PASSWORD_MESSAGE)?;
+
+    if password == password_confirm {
+        Ok(password)
+    } else {
+        Err(PasswordPromptError::MatchError)
+    }
+}
